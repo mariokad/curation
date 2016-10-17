8ca3bf72e4c939171f7f4b613d4e3afe5525fc0f
Marissa <marissaokada@gmail.com>
2016-10-17 10:53:40 -0700

    Create basic server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var db = require('./db');
+
+var app = express();
+module.exports = app;
+
+var port = 8000;
+
+var ip = '127.0.0.1';
+
+app.listen(port);
+console.log('Listening on http://' + ip + ':' + port);
